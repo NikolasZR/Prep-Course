8f5ae6effd58cddc9014858f2c0a5c39c22dfe08
NikolasZR <nicolasaberastegui97@gmail.com>
2022-08-09 17:45:26 -0300

    no termine los ultimos porque no entendi como hacerlo

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const var nombre = "Martin";
+const  nombre = "Martin";
 
 // Crea una variable numérica, puede ser cualquier número:
 const var numero = 7;
@@ -28,7 +28,7 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
-  
+  return "1" === "1"
 }
 
 function suma(x, y) {
@@ -74,7 +74,7 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  if ("hola" == "pero") {
+  if ("hola" === "hola") {
   return true; } 
   return false;
 
@@ -155,19 +155,21 @@ function elevar(num, exponent) {
 function redondearNumero(num) {
   // Redondea "num" al entero más próximo y devuélvelo
   // Tu código:
-  1
+  Math. round(num)
 }
 
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
-  
+  Math. ceil(num)
 }
 
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
-  
+  var x = Math.floor(Math.random()*2);
+
+console.log(x);
 }
 
 function esPositivo(numero) {
@@ -175,27 +177,35 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  
+  if ( numero == true ) {
+    console.log("es positivo")
+} 
+else {
+console.log("es negativo") }
 }
+
+  
+
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  console.log ( str + " ! " );
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  
+  console.log("Hola, soy "  + nombre +  " me apellido " + apellido  )
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  
+  console.log("Hola "  + nombre  )
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
