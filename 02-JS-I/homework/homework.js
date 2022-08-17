fdc5a8aa0b701b718de2875663b534b5cee8f28a
NikolasZR <nicolasaberastegui97@gmail.com>
2022-08-17 19:57:49 -0300

    jsI aprobado

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,10 +1,10 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const  nombre = "Martin";
+const  nuevaString = "Martin";
 
 // Crea una variable numérica, puede ser cualquier número:
-const numero = 7;
+const nuevoNum = 7;
 
 // Crea una variable booleana:
 const nuevoBool = 1<2;
@@ -28,35 +28,39 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
-  return "1" === "1"
+  return str 
 }
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código: 
-  return x + y
+  var total = x + y
+  return total
   
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
-  return x - y
+  var resta = x - y
+  return resta
   
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
-  return x * y 
+  var total = x * y
+  return total 
   
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
-  return x / y 
+  var total = x / y 
+  return total
   
 }
 
@@ -74,9 +78,7 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  if ("hola" === "hola") {
-  return true; } 
-  return false;
+  return str1.length === str2.length;
 
 
 } 
@@ -115,10 +117,11 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (numero % 2 === 0); {
-    return true
+  if( num % 2 === 0) {
+    return true;
+  } else {
+    return false;
   }
-  return false 
 
 }
 
@@ -126,11 +129,11 @@ function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (numero % 2 === 1); {
-    return true
+  if(num % 2 === 1) {
+    return true;
+  } else {
+    return false;
   }
-  return false 
-  
 }
 
 function elevarAlCuadrado(num) {
@@ -143,7 +146,7 @@ function elevarAlCuadrado(num) {
 function elevarAlCubo(num) {
   // Devuelve el valor de "num" elevado al cubo
   // Tu código:
-  return Math.pow (num, 3) 
+  return num * num * num 
 }
 
 function elevar(num, exponent) {
@@ -155,21 +158,19 @@ function elevar(num, exponent) {
 function redondearNumero(num) {
   // Redondea "num" al entero más próximo y devuélvelo
   // Tu código:
-  Math. round(num)
+  return Math.round(num);
 }
 
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
-  Math. ceil(num)
+  return Math. ceil(num)
 }
 
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
-  var x = Math.floor(Math.random()*2);
-
-console.log(x);
+  return Math.random();
 }
 
 function esPositivo(numero) {
@@ -177,11 +178,15 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  if ( numero == true ) {
-    console.log("es positivo")
-} 
-else {
-console.log("es negativo") }
+  if(numero === 0) {
+    return false;
+  }
+  else if(numero > 0) {
+    return "Es positivo";
+  }
+  else {
+    return "Es negativo";
+  }
 }
 
   
@@ -191,30 +196,28 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
-  console.log ( str + " ! " );
+  return str + '!';
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  console.log("Hola, soy "  + nombre +  " me apellido " + apellido  )
+  var combinado = nombre + ' ' + apellido;
+  return combinado;
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  console.log("Hola "  + nombre  )
+  return 'Hola ' + nombre + '!';
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  var lado = parseInt(prompt("ingrese medida del lado del cuadrado"));
-  let area;
-  area = lado * lado;
-  document.write("El area del cuadrado es: " + area);
+  return alto * ancho;
 }
 
 
@@ -228,10 +231,7 @@ function retornarPerimetro(lado){
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
-  var base=parseInt(prompt("Dime la base del triangulo"));
-  var altura=parseInt(prompt("Dime la altura del triangulo"));
-   
-  alert("El area del triangulo es: " + ((base * altura)/2))
+  return (base * altura)/2
 }
 
 
@@ -239,8 +239,7 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  let resultado = euro * 1.2;
-  console.log(resultado);
+  return euro * 1.2
 }
 
 
@@ -250,11 +249,13 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if(["a", "e", "i", "o", "u"].includes(char.toLowerCase())){
-    console.log("Es Vocal!")  
-  }else {
-    console.log("Dato Incorrecto!")
+  if(letra.length > 1){
+    return "Dato incorrecto"
   }
+  if(letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"){
+    return "Es vocal"
+  }
+  return "Dato incorrecto"
 }
 
 
